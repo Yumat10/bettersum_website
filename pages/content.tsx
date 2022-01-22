5fae4b06d124945ca0bf792745e7c800ee86a2c9
Yumat <mr.yuma.tanaka@gmail.com>
2022-01-22 14:58:57 -0500

    Added CSS for contact page

--- pages/content.tsx
+++ pages/content.tsx
@@ -2,6 +2,8 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import { SubscribeToNewsletterForm } from "../components/shared/forms/SubscribeToNewsletterForm";
+import styles from "styles/Content.module.css";
+import fontStyles from "styles/fontStyles.module.css";
 
 const Content: NextPage = () => {
   return (
@@ -12,18 +14,26 @@ const Content: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>Content in the works.</h1>
-        <div>
-          <div>
-            <h2>Subscribe to our newsletter.</h2>
-            <p>
-              Sign up to recieve all the latets news and content. We regularly
-              create podcasts, videos , and articles in order to keep the
-              conversation growing.
-            </p>
-          </div>
-          <div>
-            <SubscribeToNewsletterForm />
+        <div className={styles["container"]}>
+          <div className={styles["inner-container"]}>
+            <h1 className={`${fontStyles["title-header"]}`}>
+              Content in the works.
+            </h1>
+            <h2
+              className={`${fontStyles["title-header"]} ${styles["subtitle-text"]}`}
+            >
+              Subscribe to our newsletter.
+            </h2>
+            <div className={styles["details-container"]}>
+              <p className={fontStyles["body-copy"]}>
+                Sign up to recieve all the latets news and content. We regularly
+                create podcasts, videos , and articles in order to keep the
+                conversation growing.
+              </p>
+              <div className={styles["subscribe-section"]}>
+                <SubscribeToNewsletterForm />
+              </div>
+            </div>
           </div>
         </div>
       </main>
