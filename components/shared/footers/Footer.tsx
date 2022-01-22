a9e1f18f0b4d898fc4baffdaab9753e616d393bd
Yumat <mr.yuma.tanaka@gmail.com>
2022-01-22 01:41:15 -0500

    Added CSS for Footer

--- components/shared/footers/Footer.tsx
+++ components/shared/footers/Footer.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
 import { Route } from "../../../types/Route";
-import styles from "./Footer.module.css";
 import { FooterLinksColumn } from "./FooterLinksColumn";
+import styles from "./Footer.module.css";
+import fontStyles from "styles/fontStyles.module.css";
 
 export const Footer = (): JSX.Element => {
   const footerLinks: Route[][] = [
@@ -54,47 +55,60 @@ export const Footer = (): JSX.Element => {
     ],
   ];
 
+  const SocialMediaIcons = (
+    <div className={styles["social-media-icons-container"]}>
+      <Image
+        src="/socialMediaLogos/snapchat.svg"
+        alt="snapchat"
+        width={34}
+        height={34}
+      />
+      <Image
+        src="/socialMediaLogos/instagram.svg"
+        alt="instagram"
+        width={34}
+        height={34}
+      />
+      <Image
+        src="/socialMediaLogos/tiktok.svg"
+        alt="tiktok"
+        width={30}
+        height={34}
+      />
+      <Image
+        src="/socialMediaLogos/facebook.svg"
+        alt="facebook"
+        width={34}
+        height={34}
+      />
+    </div>
+  );
+
   return (
-    <div style={{ background: "gray" }}>
-      <div>
-        <div>
+    <div className={styles["container"]}>
+      <div className={styles["inner-container"]}>
+        <div className={styles["footer-info"]}>
           <div>
-            <h2>Bettersum</h2>
+            <Image
+              src="/brandLogos/betterSumLogoBeige.svg"
+              alt="BetterSum"
+              height={40}
+              width={210}
+            />
+            {SocialMediaIcons}
           </div>
-          <div>
-            <Image
-              src="/socialMediaLogos/snapchat.svg"
-              alt="snapchat"
-              width={34}
-              height={34}
-            />
-            <Image
-              src="/socialMediaLogos/instagram.svg"
-              alt="instagram"
-              width={34}
-              height={34}
-            />
-            <Image
-              src="/socialMediaLogos/tiktok.svg"
-              alt="tiktok"
-              width={30}
-              height={34}
-            />
-            <Image
-              src="/socialMediaLogos/facebook.svg"
-              alt="facebook"
-              width={34}
-              height={34}
-            />
+
+          <div className={styles["footer-links-containers"]}>
+            <FooterLinksColumn header="Home" linksArray={footerLinks[0]} />
+            <FooterLinksColumn header="Services" linksArray={footerLinks[1]} />
+            <FooterLinksColumn header="Company" linksArray={footerLinks[2]} />
           </div>
         </div>
-        <div>
-          <FooterLinksColumn header="Home" linksArray={footerLinks[0]} />
-          <FooterLinksColumn header="Services" linksArray={footerLinks[1]} />
-          <FooterLinksColumn header="Company" linksArray={footerLinks[2]} />
+        <div className={`${fontStyles["flair-copy"]} ${styles["footer-text"]}`}>
+          Atlanta Based.{" "}
+          <span className={fontStyles["body-copy"]}>Human Centered.</span>
         </div>
       </div>
-      <div>Atlanta Based. Human Centered.</div>
     </div>
   );
 };
--- components/shared/footers/FooterLinksColumn.tsx
+++ components/shared/footers/FooterLinksColumn.tsx
@@ -1,5 +1,6 @@
 import { Route } from "../../../types/Route";
 import styles from "./FooterLinksColumn.module.css";
+import fontStyles from "styles/fontStyles.module.css";
 
 export const FooterLinksColumn = ({
   header,
@@ -9,13 +10,14 @@ export const FooterLinksColumn = ({
   linksArray: Route[];
 }): JSX.Element => {
   return (
-    <div>
-      <h3>{header}</h3>
+    <div className={styles["container"]}>
+      <h3 className={fontStyles["body-copy"]}>{header}</h3>
       {linksArray.map(({ name, path }) => (
         <a
           key={path}
           href={path}
           referrerPolicy="strict-origin-when-cross-origin"
+          className={fontStyles["flair-copy"]}
         >
           {name}
         </a>
