fdb8265d97d58f5dad1f75eff69513717141d741
Yumat <mr.yuma.tanaka@gmail.com>
2022-01-23 13:03:48 -0500

    Added responsiveness to team page

--- components/pages/teamPage/TeamMemberColumn.tsx
+++ components/pages/teamPage/TeamMemberColumn.tsx
@@ -9,25 +9,49 @@ export const TeamMemberColumn = ({
   bio,
   profileImage,
 }: TeamMemberIntro): JSX.Element => {
-  return (
-    <div className={styles["container"]}>
-      <div className={styles["image-container"]}>
-        <Image
-          src={profileImage}
-          alt={`${fullName} Profile Image`}
-          objectFit="cover"
-          layout="fill"
-        />
-      </div>
-      <div className={styles["text-container"]}>
-        <h2 className={fontStyles["category-header"]}>{fullName}</h2>
-        <h3 className={`${fontStyles["body-copy"]} ${styles["title-text"]}`}>
-          {title}
-        </h3>
-        <p className={`${fontStyles["flair-copy"]} ${styles["bio-text"]}`}>
-          {bio}
-        </p>
-      </div>
+  const profileImageComponent = (
+    <div className={styles["image-container"]}>
+      <Image
+        src={profileImage}
+        alt={`${fullName} Profile Image`}
+        objectFit="cover"
+        layout="fill"
+      />
     </div>
+  );
+
+  const nameComponent = (
+    <h2 className={fontStyles["category-header"]}>{fullName}</h2>
+  );
+
+  const titleComponent = (
+    <h3 className={`${fontStyles["body-copy"]} ${styles["title-text"]}`}>
+      {title}
+    </h3>
+  );
+
+  const bioComponent = (
+    <p className={`${fontStyles["flair-copy"]} ${styles["bio-text"]}`}>{bio}</p>
+  );
+
+  return (
+    <>
+      <div className={styles["container"]}>
+        {profileImageComponent}
+        <div className={styles["text-container"]}>
+          {nameComponent}
+          {titleComponent}
+          {bioComponent}
+        </div>
+      </div>
+      <div className={styles["mobile-container"]}>
+        {profileImageComponent}
+        <span>
+          {nameComponent}
+          {titleComponent}
+        </span>
+        {bioComponent}
+      </div>
+    </>
   );
 };
