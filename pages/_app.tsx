d9b139b3353d2d3e0814299d39a94611b4223a64
Yumat <mr.yuma.tanaka@gmail.com>
2022-01-28 23:46:10 -0500

    Changed loading animations to only appear when on home page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,22 @@ import { Navbar } from "../components/shared/navbars/Navbar";
 import { Footer } from "../components/shared/footers/Footer";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import {
+  homePageAnimationDuration,
+  homePageLoadDuration,
+} from "animations/homePageAnimations";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   useEffect(() => {
-    document.body.style.overflow = "hidden";
-    setTimeout(() => (document.body.style.overflow = "auto"), 3000);
+    if (router.route === "/") {
+      window.scrollTo({ top: 0, behavior: "smooth" });
+      document.body.style.overflow = "hidden";
+      setTimeout(
+        () => (document.body.style.overflow = "auto"),
+        (homePageLoadDuration + homePageAnimationDuration) * 1000
+      );
+    }
   }, []);
 
   return (
