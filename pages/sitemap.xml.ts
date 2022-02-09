d71c51eeaadbb1f3cafc4cb410164038db6ea086
Yumat <mr.yuma.tanaka@gmail.com>
2022-02-09 11:39:49 -0500

    Initial attempt at SEO (robots.txt, sitemap.txt, Google Analytics)

---
+++ pages/sitemap.xml.ts
@@ -0,0 +1,59 @@
+import { NextApiResponse } from "next";
+
+//pages/sitemap.xml.js
+
+function generateSiteMap(): string {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://www.bettersum.com</loc>
+     </url>
+     <url>
+       <loc>https://www.bettersum.com/services</loc>
+     </url>
+     <url>
+       <loc>https://www.bettersum.com/team</loc>
+     </url>
+     <url>
+       <loc>https://www.bettersum.com/content</loc>
+     </url>
+     <url>
+       <loc>https://www.bettersum.com/contact</loc>
+     </url>
+    </urlset>`;
+  //  ${posts
+  //    .map(({ id }) => {
+  //      return `
+  //    <url>
+  //        <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+  //    </url>
+  //  `;
+  //    })
+  //    .join("")}
+}
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
+export async function getServerSideProps({ res }: { res: NextApiResponse }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap();
+
+  res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
