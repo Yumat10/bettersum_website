7a9dbe78238f8427d741a9e62a364ad73f2b8376
Yumat <mr.yuma.tanaka@gmail.com>
2022-02-25 17:30:27 -0500

    Added CSS to CSTOQuote

--- components/pages/workPage/templates/caseStudyTemplateOne/CSTOQuote.tsx
+++ components/pages/workPage/templates/caseStudyTemplateOne/CSTOQuote.tsx
@@ -1,5 +1,6 @@
 import { useCaseStudyContext } from "contexts/caseStudyContext";
 import Image from "next/image";
+import fontStyles from "styles/fontStyles.module.css";
 import styles from "./CSTOQuote.module.css";
 
 export const CSTOQuote = (): JSX.Element | null => {
@@ -19,25 +20,36 @@ export const CSTOQuote = (): JSX.Element | null => {
   return (
     <div className={styles["container"]}>
       <div className={styles["quote-container"]}>
-        <h3 className={styles["quote"]}>{quote}</h3>
+        <h3 className={`${fontStyles["title-header"]} ${styles["quote"]}`}>
+          {quote}
+        </h3>
         <div className={styles["quote-details-container"]}>
-          <p>{quoteUserName} </p>
-          <p>@ </p>
-          <p>{quoteUserTitle}</p>
-          <Image
-            src="/arrows/leftArrowBeige.svg"
-            alt="Go"
-            width={24}
-            height={24}
-          />
-          <a href={quoteLink} target="_blank" rel="noreferrer">
-            {quoteLinkText}
+          <p className={fontStyles["flair-copy"]}>
+            <b>{quoteUserName} </b>
+          </p>
+          <p className={fontStyles["flair-copy"]}>@ </p>
+          <p className={fontStyles["flair-copy"]}>{quoteUserTitle}</p>
+          <div className={styles["arrow"]}>
+            <Image
+              src="/arrows/rightArrowBeige.svg"
+              alt="Go"
+              width={24}
+              height={24}
+            />
+          </div>
+          <a
+            href={quoteLink}
+            target="_blank"
+            rel="noreferrer"
+            className={fontStyles["flair-copy"]}
+          >
+            <b>
+              <u>{quoteLinkText}</u>
+            </b>
           </a>
         </div>
       </div>
-      <div className={styles["video-container"]}>
-        <video src={video.url} autoPlay muted loop />
-      </div>
+      <video src={video.url} autoPlay muted loop className={styles["video"]} />
     </div>
   );
 };
